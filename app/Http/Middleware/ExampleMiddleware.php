656dbfe09970eb0a1d651d5242f1374520f43f37
Amna Saleem <saleemamna431@gmail.com>
2024-10-03 00:18:47 -0700

    hi commit

---
+++ app/Http/Middleware/ExampleMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ExampleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if ($request->pass != 1212) {
+            echo "Wrong Password";
+            die;
+        }
+        return $next($request);
+    }
+}
